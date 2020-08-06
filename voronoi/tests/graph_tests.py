b694f260fd138c72719c2f3842bdd26a7d4513e5
Mathias <mathiaswi177@gmail.com>
2020-08-06 01:39:18 -0700

    Changed test for vector norm

--- voronoi/tests/graph_tests.py
+++ voronoi/tests/graph_tests.py
@@ -8,21 +8,29 @@ from graph.coordinate import Coordinate
 from graph.point import Point
 from graph.algebra import Algebra
 
+#Test that a Coordinate can initialize correctly
 coord = Coordinate(2, 2)
 assert coord.x == 2
 assert coord.y == 2
 
+#Test that a Point can initialize correctly
 point1 = Point(1, 0, None, 'firstPoint', None)
 assert point1.name == 'firstPoint'
 
+#Test Algebra.distance(point_1, point_2) calculates correct distance
 point2 = Point(0, 0, None, 'secondPoint', None)
-
 dist = Algebra.distance(point1, point2)
 assert dist == 1.0
 
+#Test Algebra.magnitude(vector) calculates correct magnitude of a vector
 vector = [4, 0]
-norm = Algebra.magnitude(vector)
-assert norm == 4.0
+magnitude = Algebra.magnitude(vector)
+assert magnitude == 4.0
+
+#Test Algebra.norm(vector) calculates correct norm of a vector
+norm = Algebra.norm(vector)
+normSum = sum(norm)
+assert normSum == 1.0
 
 print('All graph tests passed')
 
